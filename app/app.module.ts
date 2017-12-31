fcb903d4d3d257cc2775f3b4a414ece2d4e21657
Karan Sharma <cserkaran@gmail.com>
2017-12-31 16:55:44 +1100

    Finished module 3

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { EventsAppComponent } from "./event-app.component";
+import { EventsListComponent } from "./events/events-list.component";
+import { EventThumbnailComponent } from "./events/event-thumbnail.component";
+import { NavBarComponent } from './nav/navbar.component'
 
 @NgModule({
     imports: [
         BrowserModule
     ], 
     declarations: [
-        EventsAppComponent
+        EventsAppComponent,
+        EventsListComponent,
+        EventThumbnailComponent,
+        NavBarComponent
     ], 
     bootstrap: [
         EventsAppComponent
--- app/event-app.component.ts
+++ app/event-app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core'
 
 @Component({
-    template: "<h2>Hello World</h2>",
+    template: `
+    <nav-bar></nav-bar>
+    <events-list></events-list>`,
     selector: 'events-app'
 })
 export class EventsAppComponent{
